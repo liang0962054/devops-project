5a7919d85f696c3dc94451901fd60e067ef151ca
liang0962054 <liang0962054@gmail.com>
2025-05-29 15:36:57 +0800

    terraform exercise

---
+++ terraform lab/Exercise7/Keypair.tf
@@ -0,0 +1,8 @@
+resource "aws_key_pair" "dove-key" {
+  key_name   = "dove-key"
+  public_key = "<my public key>"
+
+resource "aws_key_pair" "test-key" {
+  key_name   = "test-key"
+  public_key = "<my public key>"
+}
